23e8430dc4a4563da90824cb1a7a23e34e6c91b6
architasaha21 <archita.saha2106@gmail.com>
2025-12-26 16:36:13 +0530

    setup Appwrite database and habits table

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,7 +1,7 @@
 import { Text, View } from "react-native";
 // import { Link } from "expo-router";
 
-export default function StreaksScreen() {
+export default function AddHabitScreen() {
   return (
     <View>
       <Text>Hello youre on login page</Text>
